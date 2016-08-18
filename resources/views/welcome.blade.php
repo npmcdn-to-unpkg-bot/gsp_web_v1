00410ef7030522fae032c613ae0f40045a872c4f
Nate Williams <natepaulwilliams@gmail.com>
2016-08-18 14:09:42 -0600

    update php main view with config paths

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css" />
         <link rel="stylesheet" type="text/css" href="css/app.css" />
         <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=<?php echo env('MAPS_API_KEY'); ?>&libraries=geometry"></script>
-        <script src="env.js"></script>
+        <script src="<?php echo env('APP_RELATIVE_URL'); ?>/env.js"></script>
         <!-- 1. Load libraries -->
         <!-- Polyfill(s) for older browsers -->
         <!-- use gulp elixir script task to put into a vendor.js
@@ -15,9 +15,9 @@
         <script src="zone.js/dist/zone.js"></script>
         <script src="reflect-metadata/Reflect.js"></script>
         <script src="systemjs/dist/system.src.js"></script>-->
-        <script src="vendor.js"></script>
+        <script src="<?php echo env('APP_RELATIVE_URL'); ?>/vendor.js"></script>
         <!-- 2. Configure SystemJS TODO:NW minify with above in gulp -->
-        <script src="systemjs.config.js"></script>
+        <script src="<?php echo env('APP_RELATIVE_URL'); ?>/systemjs.config.js"></script>
         <script>
         System.import('app').catch(function(err){ console.error(err); });
         </script>
